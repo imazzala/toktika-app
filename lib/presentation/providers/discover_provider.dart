fa40eedc5a54410dfba9fc261134ce9de769cb4b
Iván Mazzalay <ivanmazzalay@gmail.com>
2023-05-22 21:32:32 -0300

    Final details

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:tok_tik/domain/entities/video_post.dart';
 import 'package:tok_tik/domain/repositories/video_posts_repository.dart';
-import 'package:tok_tik/infrastructure/models/local_video_model.dart';
-import 'package:tok_tik/shared/data/local_video_post.dart';
+//import 'package:tok_tik/infrastructure/models/local_video_model.dart';
+//import 'package:tok_tik/shared/data/local_video_post.dart';
 
 class DiscoverProvider extends ChangeNotifier {
-  //TODO: Repository, DataSource
-
   final VideoPostsRepository videosRepository;
 
   bool initialLoading = true;
